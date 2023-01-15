0e76dd37cae2a9c41df87c30d73d47ef5204add4
danimal141 <hideaki.ishii1204@gmail.com>
2023-01-15 13:12:05 +0900

    Add a03

---
+++ tessoku-book/a03/main.py
@@ -0,0 +1,14 @@
+import sys
+
+n, k = map(int, input().split())
+p = list(map(int, input().split()))[:n]
+q = list(map(int, input().split()))[:n]
+
+# complexity is O(N^2)
+# there is no problem because N <= 100 in this problem
+for i in p:
+    for j in q:
+        if i + j == k:
+            print("Yes")
+            sys.exit()
+print("No")
