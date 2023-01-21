16b6e4f71c7da03dafe9af3674607fa57e78c08d
danimal141 <hideaki.ishii1204@gmail.com>
2023-01-21 23:16:04 +0900

    Add a07

---
+++ tessoku-book/a07/main.py
@@ -0,0 +1,23 @@
+# sample
+# 8
+# 5
+# 2 3
+# 3 6
+# 5 7
+# 3 7
+# 1 5
+
+from itertools import accumulate
+
+d = int(input())  # days
+n = int(input())  # attendance number
+diff_list = [0] * (d + 1)
+
+for _ in range(n):
+    start, end = map(int, input().split())
+    diff_list[start] += 1
+    diff_list[end + 1] -= 1
+cs = list(accumulate(diff_list))
+
+for i in range(1, len(cs)):
+    print(cs[i])
