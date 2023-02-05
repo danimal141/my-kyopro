04b77b3e3e396dfe4ed775167465d55627a6fb41
danimal141 <hideaki.ishii1204@gmail.com>
2023-02-05 18:02:05 +0900

    Fix a18

--- tessoku-book/a18/main.py
+++ tessoku-book/a18/main.py
@@ -1,9 +1,11 @@
 n, s = map(int, input().split())
-a = [0] + list(map(int, input().split()))
+a = list(map(int, input().split()))
 
 # dynamic programming (i = 0)
 dp = [[None] * (s + 1) for _ in range(n + 1)]
 dp[0][0] = True
+for i in range(1, s + 1):
+    dp[0][i] = False
 
 # dynamic programming (i >= 1)
 # flake8: noqa
@@ -15,7 +17,7 @@ for i in range(1, n + 1):
             else:
                 dp[i][j] = False
         if j >= a[i - 1]:
-            if dp[i - 1][j] == True or dp[i - 1][j - a[i]] == True:
+            if dp[i - 1][j] == True or dp[i - 1][j - a[i - 1]] == True:
                 dp[i][j] = True
             else:
                 dp[i][j] = False
