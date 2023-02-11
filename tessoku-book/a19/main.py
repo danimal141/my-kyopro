7657784e822506109bdc14c59bf5f68e1af2f0c8
danimal141 <hideaki.ishii1204@gmail.com>
2023-02-11 19:31:55 +0900

    Add a19

---
+++ tessoku-book/a19/main.py
@@ -0,0 +1,32 @@
+"""
+n, w = 4 7
+
+w, v
+3 13
+3 17
+5 29
+1 10
+
+w = [0, 3, 3, 5, 1]
+v = [0, 13, 17, 29, 10]
+"""
+
+n, w = map(int, input().split())
+weights = [0] * (n + 1)
+values = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    weights[i], values[i] = map(int, input().split())
+
+# dynamic programming
+dp = [[0] * (w + 1) for _ in range(n + 1)]
+dp[0][0] = 0
+
+for i in range(1, n + 1):
+    for j in range(0, w + 1):
+        if j < weights[i]:
+            dp[i][j] = dp[i - 1][j]
+        elif j >= weights[i]:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i])
+
+print(max(dp[n]))
