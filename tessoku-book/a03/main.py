b540fab8df5842e8d37806cd4572a4d66a54fcb4
danimal141 <hideaki.ishii1204@gmail.com>
2023-02-13 09:50:42 +0900

    Improve comment

--- tessoku-book/a03/main.py
+++ tessoku-book/a03/main.py
@@ -5,7 +5,7 @@ p = list(map(int, input().split()))[:n]
 q = list(map(int, input().split()))[:n]
 
 # complexity is O(N^2)
-# there is no problem because N <= 100 in this problem
+# there is no problem because N <= 100 in this question
 for i in p:
     for j in q:
         if i + j == k:
